313852d3e51be30b6cc3a35f7869d559ba89ca68
AsimNet <hlh132132@gmail.com>
2015-10-27 20:30:43 +0300

    Initial Commit

---
+++ smsGate/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  smsGate
+//
+//  Created by Asim al twijry on 10/27/15.
+//  Copyright © 2015 AsimNet. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
